e53ce18fafccb9cf75a32f0069aca5e235264e32
Mohammedshuhaib <mohammedshuhaib100@gmail.com>
2022-08-20 12:01:19 +0530

    commited on aug20 12:00

--- frontend/src/components/admin/AdminHome/featured/Featured.jsx
+++ frontend/src/components/admin/AdminHome/featured/Featured.jsx
@@ -1,21 +1,36 @@
-import { MoreVert } from '@mui/icons-material'
-import React from 'react'
-import './Featured.scss'
+import { MoreVert } from "@mui/icons-material";
+import React from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import "./Featured.scss";
 
 function Featured() {
   return (
-    <div className='featured'>
-        <div className="top">
-            <h1 className="title">Total Revenue</h1>
-            <MoreVert fontSize='small'/>
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVert fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="freaturedChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
         </div>
-        <div className="bottom">
-            <div className="freaturedChart">
-                
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="disc">
+          Previous transaction processing .Last payment may not be included
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <div className="resultAmount">$12.4k</div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Featured
+export default Featured;
